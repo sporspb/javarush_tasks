5f74c593e4ca0ffea28782fff91921729ed4c34c
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-02-02 20:55:05 +0300

    added_tasks_1206-1210

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 5;
+        Integer b = (Integer) a;
+        print(a);
+        print(b);
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int a) {}
+    public static void print(Integer a) {}
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int a) {}
+    public static void print(double a) {}
+    public static void print(byte a) {}
+    public static void print(short a) {}
+    public static void print(long a) {}
+}
