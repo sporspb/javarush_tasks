9fa26fe23643ffb01633d8cbdd4f82d64dc2fcc1
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-02-03 21:36:09 +0300

    added_task12_16-20

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+//add interfaces here - добавь интерфейсы тут
+    public interface CanRun {
+        public void RUN();
+    }
+    public interface CanFly {
+        public void FLY();
+    }
+    public interface CanSwim {
+        public void SWIM();
+    }
+
+}
