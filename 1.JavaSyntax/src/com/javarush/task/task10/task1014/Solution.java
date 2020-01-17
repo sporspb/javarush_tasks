b90fdc057f590ae24709ca7540be533f4f82f6fe
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2020-01-17 17:26:58 +0300

    add some tasks

---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1014/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task10.task1014;
+
+/* 
+Расставьте минимум static-ов
+*/
+
+public class Solution {
+    public int A = 5;
+    public static int B = 5;
+    public static int C = 5;
+    public static int D = 5;
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.A = 5;
+        solution.B = 5 * B;
+        solution.C = 5 * C * D;
+        Solution.D = 5 * D * C;
+
+        Solution.D = 5;
+        System.out.println("ggg"+2+1);
+    }
+
+    public int getA() {
+        return A;
+    }
+
+}
